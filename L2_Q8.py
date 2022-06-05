cd2eb15098b8c4098e345a63af0e502549f5907a
Gopika-gs <gsgopika00@gmail.com>
2022-06-05 17:43:43 +0530

    files added

---
+++ L2_Q8.py
@@ -0,0 +1,3 @@
+mytuple = ("apple", "banana", "cherry","strawberry","kiwi","orange","grapes","water melon","berry")
+print("The fourth element in the tuple is",mytuple[3])
+print("The fourth element from last ", mytuple[-4])
